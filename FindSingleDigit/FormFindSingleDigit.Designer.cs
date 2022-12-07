4a893734bb7802170d9865d37a664e34fc23609c
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-07 12:18:31 +0200

    Add project files.

---
+++ FindSingleDigit/FormFindSingleDigit.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FindSingleDigit
+{
+    partial class FormFindSingleDigit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculateSignleDigit = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCalculateSignleDigit
+            // 
+            this.BtnCalculateSignleDigit.Location = new System.Drawing.Point(39, 43);
+            this.BtnCalculateSignleDigit.Name = "BtnCalculateSignleDigit";
+            this.BtnCalculateSignleDigit.Size = new System.Drawing.Size(180, 63);
+            this.BtnCalculateSignleDigit.TabIndex = 0;
+            this.BtnCalculateSignleDigit.Text = "Calculate Single Digit";
+            this.BtnCalculateSignleDigit.UseVisualStyleBackColor = true;
+            this.BtnCalculateSignleDigit.Click += new System.EventHandler(this.BtnCalculateSignleDigit_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(245, 43);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(484, 357);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormFindSingleDigit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCalculateSignleDigit);
+            this.Name = "FormFindSingleDigit";
+            this.Text = "Find The Single Digit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculateSignleDigit;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ FindSingleDigit/FormFindSingleDigit.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FindSingleDigit
+{
+    public partial class FormFindSingleDigit : Form
+    {
+        public FormFindSingleDigit()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculateSignleDigit_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\onedigit.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    TxtResult.Text += "Sum of digits in " + line + " = " + CalculateSingleDigit(line) + Environment.NewLine;
+                }
+            }
+        }
+
+        private int CalculateSingleDigit(string input)
+        {
+            int total = 0;
+
+            //Find the sum of all digits.
+            for (int i = 0; i < input.Length; i++)
+            {
+                total += Convert.ToInt32(input.Substring(i, 1));
+            }
+
+            //Repeat until the sum is a single digit.
+            while (total > 9)
+            {
+                string newNumber = total.ToString();
+                int newTotal = 0;
+
+                for (int i = 0; i < newNumber.Length; i++)
+                {
+                    newTotal += Convert.ToInt32(newNumber.Substring(i, 1));
+                }
+
+                total = newTotal;
+            }
+            return total;
+        }
+    }
+}
